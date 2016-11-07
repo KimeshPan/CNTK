ee5f1af4dfc940250572acb355af5cd69ab3386d
Eldar Akchurin <eldak@microsoft.com>
2016-11-07 13:56:34 +0100

    Fixing some synchronization of block momentum

--- Source/CNTKv2LibraryDll/API/CNTKLibrary.h
+++ Source/CNTKv2LibraryDll/API/CNTKLibrary.h
@@ -3668,6 +3668,7 @@ namespace CNTK
     /// A collection of additional information needed for the distributed trainer to aggregate the gradients
     struct MinibatchInfo
     {
+        bool last;
         size_t numberOfSamples;
         NDArrayViewPtr trainingLossValue;
         NDArrayViewPtr evalCriterionValue;
